b63aeca318531cd65efbc1f0c2105ae1e1c1d9ba
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2023-01-28 14:56:50 +0200

    Add files via upload

---
+++ 1. Basic Syntax, Conditional Statements and Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_sharp_traingng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a");
+            Console.Write("b");
+        }
+    }
+}

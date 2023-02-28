965dbface08efc88bf1beb75b08f92accfb6eb0d
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-02-28 21:15:15 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _9._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= i; i += 2)
+            {
+                if (count == numberOfNumbers)
+                {
+                    Console.WriteLine($"Sum: {sum}");
+                    break;
+                }
+                Console.WriteLine(i);
+                sum += i;
+                count++;
+            }
+        }
+    }
+}

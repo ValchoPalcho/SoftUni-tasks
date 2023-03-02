1c1340986464a25f1b717550683e26f962c7938f
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-03-02 22:33:25 +0200

    Add files via upload

---
+++ 03. More Exercise: Basic Syntax, Conditional Statements and Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int letters = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 0; i < letters; i++)
+            {
+                string input = Console.ReadLine();
+                int digitLength = input.Length;
+                int digit = input[0] - '0';
+                int offset = (digit - 2) * 3;
+
+                if (digit == 0)
+                {
+                    message += (char)(digit + 32);
+                    continue;
+                }
+
+                if (digit == 8 || digit == 9)
+                {
+                    offset++;
+                }
+                int letterIndex = offset + digitLength - 1;
+                message += (char)(letterIndex + 97);
+            }
+            Console.WriteLine(message);
+        }
+    }
+}

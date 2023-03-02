ff0acc445c4143b8361411759731e81931a871ca
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-03-02 22:25:56 +0200

    Add files via upload

---
+++ 02. Exercise: Basic Syntax, Conditional Statements and Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _8._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

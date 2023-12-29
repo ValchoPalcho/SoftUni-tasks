37b902e4b250c1e6c4a726d09081e59fe4d1b02e
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-12-29 18:54:08 +0200

    Add files via upload

---
+++ 04. Data Types and Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _07._Concat_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine(name1 + symbol + name2);
+        }
+    }
+}

3f0da6e9cef1942981f41609d91aca255bcaf380
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-12-29 18:56:11 +0200

    Add files via upload

---
+++ 04. Data Types and Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char character = char.Parse(Console.ReadLine());
+
+            if (char.IsUpper(character))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}

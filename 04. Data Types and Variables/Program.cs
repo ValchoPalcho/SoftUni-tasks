3ed41139118fbcb23c7164d69792cbe7b0c44f47
Valentin Karakashev <116033694+valentinkarakashev@users.noreply.github.com>
2023-12-29 18:52:40 +0200

    Add files via upload

---
+++ 04. Data Types and Variables/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = i;
+                int sum = 0;
+
+                while (number > 0)
+                {
+                    int digit = number % 10;
+                    sum += digit;
+                    number /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
